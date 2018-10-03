2f8e97cd259878147c027ccebcf2dd8fab912377
Leonardo Zanela <leonardozaneladias@gmail.com>
2018-10-03 16:50:54 -0300

    Exercício 7.2

--- app/src/main/java/br/com/caelum/leozd/fragment/ListaLivrosFragment.java
+++ app/src/main/java/br/com/caelum/leozd/fragment/ListaLivrosFragment.java
@@ -12,6 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.mugen.Mugen;
 import com.mugen.MugenCallbacks;
 import com.mugen.attachers.BaseAttacher;
@@ -21,6 +24,7 @@ import java.util.List;
 
 import br.com.caelum.leozd.R;
 import br.com.caelum.leozd.adapter.LivroAdapter;
+import br.com.caelum.leozd.adapter.LivroInvertidoAdapter;
 import br.com.caelum.leozd.modelo.Livro;
 import br.com.caelum.leozd.server.WebClient;
 import butterknife.BindView;
@@ -32,10 +36,27 @@ public class ListaLivrosFragment extends Fragment {
 
     private List<Livro> livros = new ArrayList<>();
 
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
     @BindView(R.id.lista_livros)
     RecyclerView recyclerView;
     private boolean carregando;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        mFirebaseRemoteConfig.setDefaults(R.xml.remote_config);
+
+        mFirebaseRemoteConfig.fetch(15).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    mFirebaseRemoteConfig.activateFetched();
+                }
+            }
+        });
+    }
 
     @Nullable
     @Override
@@ -45,7 +66,7 @@ public class ListaLivrosFragment extends Fragment {
 
         ButterKnife.bind(this, view);
 
-        recyclerView.setAdapter(new LivroAdapter(livros));
+
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
@@ -84,6 +105,12 @@ public class ListaLivrosFragment extends Fragment {
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         activity.getSupportActionBar().setTitle("Catalogo");
+
+        if(mFirebaseRemoteConfig.getBoolean("idioma")){
+            recyclerView.setAdapter(new LivroInvertidoAdapter(livros));
+        }else {
+            recyclerView.setAdapter(new LivroAdapter(livros));
+        }
 
     }
 
